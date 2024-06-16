a69cc6dfbe3d67a69601eac954cc2514c5ece6af
EdersonHTT <edersondasilvaschmidt@gmail.com>
2024-06-16 16:06:09 -0300

    sistema de loja e vender

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let comecar = Number(prompt(`Começar\n[1] Sim\n[2] Sim`))
+
+if(comecar === 1){
 function batalha (ini){//função batalha
     let batalhar
     let danoPlayer
@@ -13,8 +16,7 @@ function batalha (ini){//função batalha
         while(jogador.hp > 0 && ini.hp > 0 && batalhar !== 2){
             if(batalhar === undefined){
                 batalhar = Number(prompt(`Em batalha:\n[1] Atacar\n[2] Fugir`))
-            }
-            if(batalhar === 1){
+            } else if(batalhar === 1){
                 let atacar = Number(prompt(`[1] Bater\n[2] Magia\n[3] Voltar`))
 
                 if(atacar === 1){
@@ -23,10 +25,10 @@ function batalha (ini){//função batalha
                     taxaDano = Math.floor(Math.random()*danoPlayer)
                     taxaIni = Math.floor(Math.random()*danoIni)
     
-                    if(taxaDano > danoPlayer/ini.des){
+                    if(taxaDano > danoPlayer - ini.des){
                         ini.hp -= danoPlayer
                     }
-                    if(taxaIni > danoIni/jogador.des){
+                    if(taxaIni > danoIni - jogador.des){
                         jogador.hp -= danoIni
                     }
                 }else if(atacar === 2){
@@ -48,8 +50,15 @@ function batalha (ini){//função batalha
                     }
                 } else if(atacar === 3){
                     batalhar = undefined
+
+                } else {
+                    batalhar = undefined
+
                 }
+            } else {
+                batalhar = 2
             }
+
             if(jogador.hp > 0 && ini.hp > 0){
                 console.clear()
                 console.log(`\n\n Vida ${jogador.nome}: ${jogador.hp}\n =========================\n Vida ${ini.nome}: ${ini.hp}`)
@@ -60,11 +69,15 @@ function batalha (ini){//função batalha
                 acao = undefined
                 console.clear()
                 drops(ini)
+                if(inimigos[inimigosMortos].hp <= 0){//prosseguir para batalhas
+                    inimigosMortos += 1
+                }
             }else if(jogador.hp <= 0){
                 console.clear()
                 console.log(`\n\n Vida ${jogador.nome}: 0\n =========================\n Vida ${ini.nome}: ${ini.hp}`)
                 alert(`===============Game Over!!===============`)
                 alert(`=============Tente novamente=============`)
+                sair = 1
                 window.location.reload()
             }
         }
@@ -81,7 +94,7 @@ function itensMonstros(ini){
 
 }
 
-function status (){
+function statu (xpM){
     let sta = `==================jogador===================
     nome: ${jogador.nome}
     lvl: ${jogador.lvl}
@@ -91,20 +104,51 @@ function status (){
     def: ${jogador.def}
     mag: ${jogador.mag}
     des: ${jogador.des}
-    xp: ${jogador.xp}`
+    xp: ${jogador.xp}/${xpM}`
     return sta
 }
 
 function inventario(inventario){
     let ordem
+    
     for(let x in inventario){
-        if(inventario[x] === inventario[0]){
+        if(Number(x) === 0){
             ordem = `[${Number(x)+1}] ${inventario[x].nome}`
-        }else if(inventario[x] !== inventario[0]){
+        }else{
             ordem += `\n[${Number(x)+1}] ${inventario[x].nome}`
         }
     }
     return ordem
+}
+
+function desEquip(desequip){
+    let parte = ["Capacete","Peitoral","Calça","Bota", "Arma"]
+    let jogClasse
+    
+    if(equipamento[desequip] !== parte[desequip]){
+        if(parte[desequip] !== parte[parte.length-1]){
+            jogador.inventario.push(equipamento[desequip])
+                jogador.def -= equipamento[desequip].def
+                equipamento[desequip] = parte[desequip]
+        }else{
+            jogador.inventario.push(equipamento[desequip])
+            if(parte[desequip] === parte[parte.length-1]){
+                if(equipamento[Number(desequip)].mag){
+                    jogClasse = `mag`
+                }else{
+                    jogClasse = `forca`
+                }
+            }
+
+            if(equipamento[desequip][jogClasse]){
+
+                jogador[jogClasse]-= equipamento[desequip][jogClasse]
+                equipamento[desequip] = parte[desequip]
+
+            }
+        }
+    }
+    return equipamento[desequip]
 }
 
 function equipamentos(equipa){
@@ -132,32 +176,9 @@ function equipamentos(equipa){
     return ordem
 }
 
-function desEquip(desequip){
-    let parte = ["Capacete","Peitoral","Calça","Bota", "Arma"]
-    jogador.inventario.push(equipamento[desequip])
-
-    if(parte[desequip] != parte[parte.length-1]){
-        alert(equipamento[desequip].nome)
-            jogador.def -= equipamento[desequip].def
-            equipamento[desequip] = parte[desequip]
-    }else{
-        if(jogador.inventario[desequip].atacar){
-
-            jogador.atacar -= equipamento[desequip].atacar
-            equipamento[desequip] = parte[desequip]
-
-        }else if(jogador.inventario[desequip].mag){
-
-            jogador.mag -= equipamento[desequip].mag
-            equipamento[desequip] = parte[desequip]
-        }
-    }
-
-    return equipamento[desequip]
-}
-
 function equipar(jog, indice){
     let jogClasse
+    let itenclasse
     let compararParte = ["Capacete","Peitoral","Calça","Bota","Arma"]
     let pegarIndice = [1,2,3,4,5]
     let indiceEquip 
@@ -169,8 +190,10 @@ function equipar(jog, indice){
     }
 
     if(jog[indice].mag){
+        itenclasse = `mag`
         jogClasse = `mag`
     }else{
+        itenclasse = `ataque`
         jogClasse = `forca`
     }
 
@@ -194,14 +217,14 @@ function equipar(jog, indice){
         if(jog[indice].classe === "Arma"){
             if(equipamento[indiceEquip].classe !== "Arma"){
                 equipamento[indiceEquip] = jog[indice]
-                jogador[jogClasse] += equipamento[indiceEquip][jogClasse]
+                jogador[jogClasse] += equipamento[indiceEquip][itenclasse]
                 jog.splice(indice, 1)
             }else{
                 jog.push(equipamento[indiceEquip])
-                jogador[jogClasse] -= equipamento[indiceEquip][jogClasse]
+                jogador[jogClasse] -= equipamento[indiceEquip][itenclasse]
                 equipamento.splice(0,1)
                 equipamento[indiceEquip] = jog[indice]
-                jogador[jogClasse] += equipamento[indiceEquip][jogClasse]
+                jogador[jogClasse] += equipamento[indiceEquip][itenclasse]
                 jog.splice(indice, 1)
             }
         }
@@ -209,13 +232,75 @@ function equipar(jog, indice){
     return equipamento[indiceEquip]
 }
 
+function loja (itens){
+    let valAle    
+
+    if(inimigosMortos === lojaIni || umaVez === 1){
+        for(let x in lojaItens){
+            lojaItens.shift()
+            lojaItens.pop()
+        }
+        lojaItens.pop()
+
+        do{
+            valAle = Math.floor(Math.random()*itens.length)
+            
+            if(lojaItens[0] === undefined){
+                lojaItens.push(valAle)
+            }else{
+                for(let x in lojaItens){
+                    if(lojaItens.includes(valAle) === false){
+                        lojaItens.push(valAle)
+                    } 
+                }
+            }
+        }while(lojaItens.length-1 < 4)
+            lojaIni += 5
+            umaVez = null
+    }
+    let ordem
+    for(let x in lojaItens){
+        if(lojaItens[x] === lojaItens[0]){
+            ordem = `[${Number(x)+1}] ${itens[lojaItens[x]].nome} ==> Preço: ${itens[lojaItens[x]].valor}`
+        }else if(lojaItens[x] !== lojaItens[0]){
+            ordem += `\n[${Number(x)+1}] ${itens[lojaItens[x]].nome} ==> Preço: ${itens[lojaItens[x]].valor}`
+        }
+    }
+    return ordem
+}
+
+function comprar (item, quatidade){
+    if(jogador.dinheiro > 0){
+        for(let x =0; x < quatidade; x++){
+            if(jogador.dinheiro > 0 && jogador.dinheiro >= itens[lojaItens[item]].valor){
+                jogador.inventario.push(itens[lojaItens[item]])
+                jogador.dinheiro -= itens[lojaItens[item]].valor
+            } else {
+                alert(`Não há dinheiro o suficiente.`)
+                x = quatidade
+            }
+        }
+    } else {
+        alert(`Não há dinheiro o suficiente.`)
+        acao = undefined
+    }
+    inventario()
+    return jogador.inventario
+}
+
+function vender(itemInven){
+    jogador.dinheiro += jogador.inventario[itemInven].valor
+    jogador.inventario.splice(itemInven, 1)
+    return jogador
+}
+
 function jogXp (){
-    let restrixp = 100
-    let subirStats = 2
-    if(jogador.xp === restrixp){
+
+    if(jogador.xp >= xpMaximo){
         jogador.lvl += 1
-        restrixp += 50
-        jogador.xp = 0
+        jogador.xp -= xpMaximo
+        xpMaximo += 50
+        
         if(jogador.lvl === subirStats){
             jogador.forca += 3
             jogador.hp += 3
@@ -225,13 +310,13 @@ function jogXp (){
             subirStats += 1
         }
     }
-    return jogador.lvl
+    return xpMaximo
 }
 
 function drops (ini){
     alert(`Recebe:\n====> +${ini.xp}`)
     jogador.xp += ini.xp
-    jogXp()
+    jogXp(xpMaximo)
     return jogador.xp
 }
 
@@ -246,27 +331,46 @@ mag: 0,
 des: 0,
 inventario: [],
 xp: 0,
-moedas: {ouro: 2, prata: 15, cobre: 30}
-
+dinheiro: 100
 }
 
-let itens = [chapeuDeMago = {
+let itens = [
+    chapeuDeMago = {
         nome: "chapeu de mago", 
         classe: "Capacete",
-        def: 2
-    },chapeu = {
+        def: 2,
+        valor: 10
+    },
+    chapeu = {
         nome: "chapeu", 
         classe: "Capacete",
-        def: 2
-    },varinha = {
+        def: 2,
+        valor: 5
+    },
+    varinha = {
         nome: "Varinha", 
         classe: "Arma",
-        mag: 15
-    },livro = {
-        nome: "Livro"
+        mag: Number(15),
+        valor: 15
+    },
+    livro = {
+        nome: "Livro",
+        valor: 5
+    },
+    espada = {
+        nome: "espada",
+        classe: "Arma",
+        ataque: Number(20),
+        valor: 15
+    },marreta = {
+        nome: "Marreta",
+        classe: "Arma",
+        ataque: Number(20),
+        valor: 15
     },]
 
-let inimigos =[rato={
+let inimigos =[
+rato={
     nome:"Mestre Splinter",
     hp: 15,
     forca: 5,
@@ -283,7 +387,32 @@ Pato={
     des: 5,
     drops:[],
     xp: 50
+},goblin={
+    nome:"Goblin",
+    hp: 15,
+    forca: 5,
+    def: 5,
+    des: 5,
+    drops:[],
+    xp: 50
+},lobo={
+    nome:"Lobo Pidão",
+    hp: 15,
+    forca: 5,
+    def: 5,
+    des: 5,
+    drops:[],
+    xp: 50
+},atumalaca={
+    nome:"Atumalaca",
+    hp: 50,
+    forca: 15,
+    def: 10,
+    des: 10,
+    drops:[],
+    xp: 150
 }]
+
 
 let equipamento = ['Capacete', 'Peitoral', 'Calça', 'Bota', 'Arma']//Guarda os itens do jogo
 let lixo = []
@@ -291,6 +420,12 @@ let acao//Para as açoes
 let inimigosMortos = 0
 let sair
 let item//para guardar um item
+let xpMaximo = 100
+let subirStats = 2
+let umaVez = 1
+let lojaItens = []
+let lojaIni = 0
+
 
 console.log("===Bem vindo(a)=====\nVamos criar seu personagem.")//Boas vindas
 
@@ -350,24 +485,24 @@ switch(esc){
 console.clear()
 console.log(`==================jogador===================
     nome: ${jogador.nome}
+    lvl: ${jogador.lvl}
     classe: ${jogador.classe}
     hp: ${jogador.hp}
     forca: ${jogador.forca}
     def: ${jogador.def}
     mag: ${jogador.mag}
     des: ${jogador.des}
+    xp: ${jogXp()}
     inventario: \n${inventario(jogador.inventario)}
      `)//Imprimir o jogador
 
 do{
-    acao = Number(prompt("Faça uma ação:\n[1] Prosseguir\n[2] Inventário\n[3] Equipamentos\n[4] Status\n[5] Sair"))
+    acao = Number(prompt("Faça uma ação:\n[1] Prosseguir\n[2] Inventário\n[3] Equipamentos\n[4] Status\n[5] loja\n[6] Sair"))
     console.clear()
     
     
     if(acao === 1){
-        if(inimigos[inimigosMortos].hp <= 0){//prosseguir para batalhas
-            inimigosMortos += 1
-        }
+        
         batalha(inimigos[inimigosMortos]) 
 
     }else if(acao === 2){
@@ -377,12 +512,13 @@ do{
             acao = Number(prompt("[1] Equipar\n[2] Vender"))
             if(acao === 1){
                 equipar(jogador.inventario, item)
+            } else if(acao === 2){
+                vender(item)
             }
         }       
     }else if(acao === 3){
         let desequipNu = Number(prompt(`Equipamentos:\n${equipamentos(equipamento)}`))-1
-
-        if(desequipNu <= 4 && desequipNu !== toString(desequipNu)){
+        if(equipamento[desequipNu]){
             let desequip
             desequip = Number(prompt(`============> Desequipar <============\n\n[1] Sim\n[2] Não`))
             if(desequip === 1){
@@ -392,12 +528,20 @@ do{
             }
         }
     }else if(acao === 4){
-        alert(status())
+        alert(statu(xpMaximo))
 
     }else if(acao === 5){
+        let compra = Number(prompt(`Comprar:\n${loja(itens)}\n============Dinheiro: ${jogador.dinheiro}=======`))-1
+        if(itens[lojaItens[compra]]){
+            let quantia = Number(prompt(`Quntos?`))
+            comprar(compra, quantia)
+        }
+
+    }else if(acao === 6){
         sair = Number(prompt(`[1] Recomeçar\n[2] Parar Jogo`))
         if(sair === 1){
             window.location.reload()
         }
     }
 }while(acao === undefined || batalha() === undefined && sair !== 2 && sair !== 1)
+}
